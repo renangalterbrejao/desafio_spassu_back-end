8d65185b1e85834d170ebd62c03a21aaeb5e2430
Renan Galter Brejão <renangalterbrejao@gmail.com>
2025-02-06 22:57:57 -0300

    Back-end - Ajustes

--- src/main/java/br/com/spassu/api/controller/RelatoriosController.java
+++ src/main/java/br/com/spassu/api/controller/RelatoriosController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import io.swagger.annotations.Api;
 
 @Api(tags = "Relatorios")
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "/relatorios", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RelatoriosController {
 	
